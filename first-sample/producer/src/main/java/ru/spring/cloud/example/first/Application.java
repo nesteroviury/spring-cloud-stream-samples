da569ffa5f22abc16e8b00bb362714748000328d
Yuri Nesterov <nesteroviury@gmail.com>
2021-12-23 19:44:55 +0300

    rest controller added

--- first-sample/producer/src/main/java/ru/spring/cloud/example/first/Application.java
+++ first-sample/producer/src/main/java/ru/spring/cloud/example/first/Application.java
@@ -1,9 +1,27 @@
 package ru.spring.cloud.example.first;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@RestController
 @SpringBootApplication
-@Controller
 public class Application {
+    private final StreamBridge streamBridge;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void delegateToSupplier(String data) {
+        streamBridge.send("source-out-0", data);
+    }
 }
